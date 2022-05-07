c290da5c10f293a7f0fa8f8464b6e353fd3b9b33
Deeptikandel <deeptikandel1@gmail.com>
2022-05-08 00:33:21 +0300

    types added to CountryReducers

--- src/redux/reducers/CountryReducer.ts
+++ src/redux/reducers/CountryReducer.ts
@@ -1,30 +1,33 @@
-// import {
-//   CountryReducersState,
-//   FETCH_COUNTRIES,
-//   FETCH_COUNTRIES_FAILURE,
-//   FETCH_COUNTRIES_SUCCESS,
-// } from '../../types/Types'
+import {
+  CountryReducersState,
+  FETCH_COUNTRIES,
+  FETCH_COUNTRIES_FAILURE,
+  FETCH_COUNTRIES_SUCCESS,
+} from '../../types/Types'
 
-const initState = {
+const initState: CountryReducersState = {
   countries: [],
   isLoading: false,
   error: '',
 }
-export default function countryReducer(state = initState, action: any) {
+export default function countryReducer(
+  state: CountryReducersState = initState,
+  action: any
+) {
   switch (action.type) {
-  case 'FETCH_COUNTRIES':
+  case FETCH_COUNTRIES:
     return {
       ...state,
       isLoading: true,
     }
-  case 'FETCH_COUNTRIES_SUCCESS':
+  case FETCH_COUNTRIES_SUCCESS:
     return {
       ...state,
       isLoading: false,
       countries: action.payload,
       error: '',
     }
-  case 'FETCH_COUNTRIES_FAILURE':
+  case FETCH_COUNTRIES_FAILURE:
     return {
       ...state,
       isLoading: false,
